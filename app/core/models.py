4f16b58cc13d9d1cd87490854732cdaf0aad300d
mhero007 <momohhero2@gmail.com>
2020-05-28 10:02:34 +0100

    fixed linting problems

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 # Create your models here.
 
 # manager class -  create user or superuser
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -19,12 +21,14 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password):
         """create and saves a new superuser"""
         user = self.create_user(email, password)
-        user.is_staff=True
-        user.is_superuser=True
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
 # create model class
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
@@ -34,4 +38,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # assign usermanager to the objects attribute
     objects = UserManager()
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
